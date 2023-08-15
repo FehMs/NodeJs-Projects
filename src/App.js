69bca990fe3e131c0a80b8d89efd2e475187b103
FehMs <felipe_morais360@hotmail.com>
2023-08-14 22:56:28 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import logoheader from './images/logo.png'
+import {BiUserCircle} from "react-icons/bi";
 
-function App() {
+function reader() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <div className="logoheader">
+        <img className="logo" src={logoheader} alt="logo"></img>
+      </div>
+      <nav className="navbar">
+        <a href="">Home</a>
+        <a href="">Sac</a>
+        <a href="">Assistência</a>
+      </nav>
+      <div className="login-wrapper">
+        <div className='icon'>
+          <BiUserCircle size={55}/>
+        </div>
+        <div className='login'>
+          <a href=''>Entrar</a>
+          <a href=''>Registrar-se</a>
+        </div>
+      </div>
     </div>
+      
   );
 }
+function banner() {
+  <div className='banner'>
+    
+  </div>
+}
 
-export default App;
+export default reader;
